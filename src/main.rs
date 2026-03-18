f72583e2ca5fe9bca02f3e4ea4600e9236198135
Godwin Addy <dev.niiaddy@gmail.com>
2026-03-18 21:48:46 +0000

    Auto-saved workspace session by Syncshot

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ fn main() {
 
     let mut camera = Camera::default();
     camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 400;
+    camera.image_width = 800;
     camera.samples_per_pixel = 100;
     camera.max_depth = 50;
 
